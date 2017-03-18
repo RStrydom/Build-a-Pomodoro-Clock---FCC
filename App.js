bb51b17fcdec597de6346e5194e9315e44b1b0a6
RStrydom <renierbs@gmail.com>
2017-03-18 14:41:49 +0200

    Bootstrap app with react-community/create-react-native-app

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.timer}>25.00</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  timer: {
+    color: '#FFF',
+    fontSize: 70
+  },
+});
